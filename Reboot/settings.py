2c79eb10958f7e0ef6fd547e7175c4ad38415c31
kavyasetava135 <kavyasetava135@gmail.com>
2026-04-17 19:01:07 +0530

    allowed ipddress

--- Reboot/settings.py
+++ Reboot/settings.py
@@ -31,7 +31,8 @@ SECRET_KEY = 'django-insecure-lg7i8v#3k(1l7l0w#8(l94#0piebbb)*#j#fvvph8$i)a92+x*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['16.170.222.16']
 
 
 # Application definition
@@ -67,6 +68,7 @@ CORS_ALLOW_HEADERS = [
     "ngrok-skip-browser-warning",
 ]
 CSRF_TRUSTED_ORIGINS = [
+     "http://16.170.222.16",
     "https://isochroous-unlidded-elvina.ngrok-free.dev"
 ]
 
@@ -76,15 +78,13 @@ CSRF_TRUSTED_ORIGINS = [
 # ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware', 
-    'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-
     'allauth.account.middleware.AccountMiddleware',
-
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
@@ -200,6 +200,7 @@ REST_FRAMEWORK = {
     ],
      "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
     ],
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
